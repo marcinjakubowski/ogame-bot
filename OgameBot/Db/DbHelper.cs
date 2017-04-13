4368e5544b1a0a6005c647eed882e0d4d85fcfbe
Marcin Jakubowski <mjakubowski@gmail.com>
2017-04-13 10:27:15 +0200

    Added db helper for common db using methods

---
+++ OgameBot/Db/DbHelper.cs
@@ -0,0 +1,20 @@
+﻿using OgameBot.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgameBot.Db
+{
+    public static class DbHelper
+    {
+        public static Coordinate GetPlanetCoordinateByCp(int cp)
+        {
+            using (BotDb db = new BotDb())
+            {
+                return db.Planets.Where(p => p.PlanetId == cp).Select(p => p.LocationId).FirstOrDefault();
+            }
+        }
+    }
+}
