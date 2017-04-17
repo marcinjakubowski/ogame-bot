f5a7fd139c4066efb10ebf3f6ec87eae26954373
Marcin Jakubowski <mjakubowski@gmail.com>
2017-04-17 22:05:28 +0200

    added inject for aggregating cargo from attacks and transports/deployments

---
+++ OgameBot/Engine/Injects/EventListTotalsInject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using OgameBot.Engine.Parsing.Objects;
+using ScraperClientLib.Engine;
+using OgameBot.Objects.Types;
+using System.Linq;
+using OgameBot.Objects;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OgameBot.Engine.Injects
+{
+    public class EventListTotalsInject : IInject
+    {
+        public string Inject(OgamePageInfo info, string body, ResponseContainer response, string host, int port)
+        {
+            if (!response.RequestMessage.RequestUri.PathAndQuery.Contains("page=eventList")) return body;
+
+            var events = response.GetParsed<EventInfo>();
+
+
+            var totalAttack = TotalFor(events.Where(ei => ei.IsReturning && ei.MissionType == MissionType.Attack));
+            var totalTransport = TotalFor(events.Where(ei => !ei.IsReturning && (ei.MissionType == MissionType.Transport || ei.MissionType == MissionType.Deployment)));
+
+            StringBuilder repl = new StringBuilder();
+
+
+            string wrapper = "<tr class='eventFleet'><td colspan=11>Total cargo from attacks: <span class='textBeefy friendly'>{0}</span></td></tr>";
+
+            if (totalAttack.Total > 0)
+            {
+                repl.Append(string.Format(wrapper, totalAttack)); 
+            }
+            if (totalTransport.Total > 0)
+            {
+                repl.Append(string.Format(wrapper, totalTransport));
+            }
+
+            if (repl.Length > 0)
+            {
+                body = body.Replace(@"<tbody>", $@"<tbody>{repl.ToString()}");
+            }
+
+            return body;
+        }
+
+        private static Resources TotalFor(IEnumerable<EventInfo> events)
+        {
+            return events.Select(ei => ei.Composition.Resources)
+                         .Aggregate(new Resources(), (x, y) => x + y);
+        }
+    }
+}
