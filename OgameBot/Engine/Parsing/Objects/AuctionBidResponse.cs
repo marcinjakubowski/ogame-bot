b74cf7e2560ebb5a2e1f167a86a5aba785cc3364
Marcin Jakubowski <mjakubowski@gmail.com>
2017-04-26 15:49:55 +0200

    removed unnecessary fields from auction bid response

--- OgameBot/Engine/Parsing/Objects/AuctionBidResponse.cs
+++ OgameBot/Engine/Parsing/Objects/AuctionBidResponse.cs
@@ -8,24 +8,11 @@ namespace OgameBot.Engine.Parsing.Objects
     {
         public bool Error { get; set; }
         public string Message { get; set; }
-
-        // key => cp
-        public Dictionary<string, Resources> PlanetResources { get; set; }
-
-        public int Honor { get; set; }
         public string NewToken { get; set; }
 
         public override string ToString()
         {
             return $"Bid: {Message}";
-        }
-        
-
-        public class Resources
-        {
-            public double Metal { get; set; }
-            public double Crystal { get; set; }
-            public double Deuterium { get; set; }
         }
     }
 }
