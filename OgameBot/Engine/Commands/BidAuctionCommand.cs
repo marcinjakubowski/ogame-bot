10aff658df32dda3dd5fbe1298de39925da91c3e
Marcin Jakubowski <mjakubowski@gmail.com>
2017-04-27 11:37:00 +0200

    improved bidding mechanism, delay when more than 5 min remaining

--- OgameBot/Engine/Commands/BidAuctionCommand.cs
+++ OgameBot/Engine/Commands/BidAuctionCommand.cs
@@ -44,24 +44,38 @@ namespace OgameBot.Engine.Commands
                 checkCount++;
                 if (status.HighestBidderId != info.PlayerId && status.MinutesRemaining != 0)
                 {
-                    int bidValue = 1 + (int)Math.Ceiling(status.Shortfall / multiplier);
+                    int bidValue = status.Shortfall;
+
+                    if (status.Shortfall < 3000) bidValue *= 2;
+                    else if (status.Shortfall < 100000) bidValue = (int)(bidValue * 1.2);
+                    else if (status.Shortfall > 200000)
+                    {
+                        Logger.Instance.Log(LogLevel.Error, $"Too high bid amount: {bidValue}, bid on your own or rerun!");
+                        return null;
+                    }
+                    bidValue += 1;
+                    int bid = (int)(Math.Ceiling(bidValue / multiplier));
 
                     var req = Client.BuildPost(new Uri("/game/index.php?page=auctioneer", UriKind.Relative), new[]
                     {
                         KeyValuePair.Create("ajax", "1"),
-                        KeyValuePair.Create($"bid[planets][{PlanetId}][metal]", BidResource == ResourceType.Metal ? bidValue.ToString() : "0"),
-                        KeyValuePair.Create($"bid[planets][{PlanetId}][crystal]", BidResource == ResourceType.Crystal ? bidValue.ToString() : "0"),
-                        KeyValuePair.Create($"bid[planets][{PlanetId}][deuterium]", BidResource == ResourceType.Deuterium ? bidValue.ToString() : "0"),
+                        KeyValuePair.Create($"bid[planets][{PlanetId}][metal]", BidResource == ResourceType.Metal ? bid.ToString() : "0"),
+                        KeyValuePair.Create($"bid[planets][{PlanetId}][crystal]", BidResource == ResourceType.Crystal ? bid.ToString() : "0"),
+                        KeyValuePair.Create($"bid[planets][{PlanetId}][deuterium]", BidResource == ResourceType.Deuterium ? bid.ToString() : "0"),
                         KeyValuePair.Create($"bid[honor]", "0"),
                         KeyValuePair.Create("token", status.Token)
                     });
 
-                    var bid = Client.IssueRequest(req);
+                    resp = Client.IssueRequest(req);
+                    last = resp.GetParsedSingle<AuctionBidResponse>(false);
 
-                    last = bid.GetParsedSingle<AuctionBidResponse>(false);
                     Logger.Instance.Log(LogLevel.Info, $"Bid {bidCount++}/{checkCount} was for {status.OwnBid + bidValue}: {last?.Message}");
                 }
-                if (!last.Error) Thread.Sleep(300);
+
+                int delay = 0 ;
+                if (status.MinutesRemaining > 5) delay = 30000;
+                Thread.Sleep(delay);
+
             } while (status.MinutesRemaining != 0);
 
             Logger.Instance.Log(status.HighestBidderId == info.PlayerId ? LogLevel.Success : LogLevel.Warning, $"Auction for {status.Item} won by {status.HighestBidderName} at {status.CurrentBid}");
