43e05b7ccec394a38063fa27b2502c1b47892051
Marcin Jakubowski <mjakubowski@gmail.com>
2017-04-27 11:16:50 +0200

    refactored commands inject to use helper methods

--- OgameBot/Engine/Injects/CommandsInject.cs
+++ OgameBot/Engine/Injects/CommandsInject.cs
@@ -21,27 +21,44 @@ namespace OgameBot.Engine.Injects
             _port = port;
             _host = host;
 
-            StringBuilder sb = new StringBuilder("$1&lt;hr/&gt;");
-            
-            sb.Append(GetLink("farm?cp=$2", "Farm")).Append(" (").Append(GetLink("farm?cp=$2&slots=0", "0 slots")).Append(")").Append(NewLine);
-            sb.Append(GetLink("hunt?cp=$2", "Hunt")).Append(", range: ").Append(GetLink("hunt?cp=$2&range=40", "40")).Append(" ").Append(GetLink("hunt?cp=$2&range=20", "20")).Append(NewLine);
-            sb.Append("Transport All: ").Append(GetLink($"transport?from=$2&to={current.PlanetId}", "From")).Append(" / ").Append(GetLink($"transport?from={current.PlanetId}&to=$2", "To")).Append(NewLine);
-            sb.Append("Fleetsave: ")
-                .Append(GetLink($"fs?cp=$2&in=360", "6h")).Append(" ")
-                .Append(GetLink($"fs?cp=$2&in=390", "6.5h")).Append(" ")
-                .Append(GetLink($"fs?cp=$2&in=420", "7h")).Append(" ")
-                .Append(GetLink($"fs?cp=$2&in=450", "7.5h")).Append(" ")
-                .Append(GetLink($"fs?cp=$2&in=480", "8h"));
+            StringBuilder sb = new StringBuilder("$1");
 
-            body = submenuRegex.Replace(body, sb.ToString());
+
+            sb.Append("<hr/>");
+            sb.Append(InjectHelper.GenerateCommandLink("farm?cp=$2", "Farm"))
+                     .Append(" (")
+                     .Append(InjectHelper.GenerateCommandLink("farm?cp=$2&slots=0", "0 slots"))
+                     .Append(")")
+                     .Append(NewLine);
+
+            sb.Append(InjectHelper.GenerateCommandLink("hunt?cp=$2", "Hunt"))
+                .Append(", range: ")
+                .Append(InjectHelper.GenerateCommandLink("hunt?cp=$2&range=40", "40"))
+                .Append(" ")
+                .Append(InjectHelper.GenerateCommandLink("hunt?cp=$2&range=20", "20"))
+                .Append(NewLine);
+
+            sb.Append("Transport All: ")
+                .Append(InjectHelper.GenerateCommandLink($"transport?from=$2&to={current.PlanetId}", "From"))
+                .Append(" / ")
+                .Append(InjectHelper.GenerateCommandLink($"transport?from={current.PlanetId}&to=$2", "To"))
+                .Append(NewLine);
+
+            sb.Append("Fleetsave: ")
+                .Append(InjectHelper.GenerateCommandLink($"fs?cp=$2&in=360", "6h"))
+                .Append(" ")
+                .Append(InjectHelper.GenerateCommandLink($"fs?cp=$2&in=390", "6.5h"))
+                .Append(" ")
+                .Append(InjectHelper.GenerateCommandLink($"fs?cp=$2&in=420", "7h"))
+                .Append(" ")
+                .Append(InjectHelper.GenerateCommandLink($"fs?cp=$2&in=450", "7.5h"))
+                .Append(" ")
+                .Append(InjectHelper.GenerateCommandLink($"fs?cp=$2&in=480", "8h"));
+
+            body = submenuRegex.Replace(body, InjectHelper.EncodeTooltip(sb.ToString()));
             return body;
         }
 
-        private string GetLink(string command, string label)
-        {
-            return $"&lt;a href=&quot;javascript:void(0)&quot; onclick=ogbcmd(&quot;{command}&quot);&gt;{label}&lt;/a&gt;";
-        }
-
-        private const string NewLine = "&lt;/br&gt;";
+        private const string NewLine = "</br>";
     }
 }
