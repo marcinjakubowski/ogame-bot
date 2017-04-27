b0162774b3bd35ad45c5601331dd79f63e353697
Marcin Jakubowski <mjakubowski@gmail.com>
2017-04-27 10:15:17 +0200

    added injecthelper with tooltip/encoding methods

---
+++ OgameBot/Engine/Injects/InjectHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace OgameBot.Engine.Injects
+{
+    public static class InjectHelper
+    {
+        public static string EncodeTooltip(string tooltip)
+        {
+            return HttpUtility.HtmlEncode(tooltip);
+        }
+
+        public static string GenerateCommandLink(string command, string label)
+        {
+            return $@"<a href=""javascript:void(0)"" onclick=ogbcmd(""{command}"")>{label}</a>";
+        }
+    }
+}
